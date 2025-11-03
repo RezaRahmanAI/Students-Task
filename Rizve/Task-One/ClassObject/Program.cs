bc0ab1d9a920956017da721042db4d5d7bda8892
Rizve Rahman Reza <rizverahman.ai@outlook.com>
2025-11-03 16:36:58 +0600

    Reza: Task one running

---
+++ Rizve/Task-One/ClassObject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+public class Car
+{
+  public string Brand { get; set; }
+  public string Model { get; set; }
+
+  public void DisplayInfo()
+  {
+    Console.WriteLine($"Car Brand: {Brand}, Model: {Model}");
+  }
+}
+
+
+class Program
+{
+  static void Main(string[] agrs)
+  {
+    Car myCar = new Car();
+    myCar.Brand = "Toyota";
+    myCar.Model = "Corolla";
+    myCar.DisplayInfo();
+  }
+}
---
+++ Rizve/Task-One/Inheritance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Employee
+{
+  public string Name { get; set; }
+
+  public void Work()
+  {
+    Console.WriteLine($"{Name} is working.");
+  }
+}
+
+public class Manager : Employee
+{
+  public void ManageTeam()
+  {
+    Console.WriteLine($"{Name} is managing the team.");
+  }
+}
+
+
+class Program
+{
+  static void Main()
+  {
+    Manager manager = new Manager();
+    manager.Name = "Alice";
+    manager.Work();
+    manager.ManageTeam();
+  }
+}
